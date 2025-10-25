050fe9e06e61845d2790104d22c2fa3d5bfc2a64
azcm12 <3269144998@qq.com>
2025-10-25 23:35:45 +0800

    feat:添加一个CategoryDto类

---
+++ src/main/java/edu/fdzc/shop/dto/CategoryDto.java
@@ -0,0 +1,13 @@
+package edu.fdzc.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+    private String categoryName;
+    private String id;
+}
